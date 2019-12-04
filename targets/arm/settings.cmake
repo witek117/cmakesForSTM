d9d9a76ee2af159ad294e2258f88a10f3b0bdf1f
witek117 <witek117@gmail.com>
2019-12-04 10:31:19 +0100

    without terminal and logger, arm with hard float flag

--- targets/arm/settings.cmake
+++ targets/arm/settings.cmake
@@ -1,18 +1,17 @@
 set (CWARN "-Wall -Wstrict-prototypes -Wextra -Werror")
 set (CXXWARN "-Wall -Wextra -Werror")
-set(CTUNING "-fomit-frame-pointer -ffunction-sections -fdata-sections")
+set (CTUNING "-fomit-frame-pointer -ffunction-sections -fdata-sections")
+set (ARMFLOAT "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
 
 ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "CPU")        # get flag -DCPU = -mcpu=cortex-mXX
-set(CMCU "-mcpu=${CPU} -mthumb -mfloat-abi=soft")
+set(CMCU "-mcpu=${CPU} -mthumb ${ARMFLOAT}")
 
 ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "C_DEFS")        # get flag -DSTM32F****
 
-string(REPLACE ";" " " C_DEFS ${C_DEFS})
-message("${C_DEFS}")
-set(RANDOM_DEFS "-DUSE_HAL_DRIVER -DSTM32F103xB")
+string(REPLACE ";" " " C_DEFS "${C_DEFS}")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu11 ${CWARN} ${CTUNING} ${CMCU} ${RANDOM_DEFS}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++17 -Dregister=\"/**/\" -fno-exceptions -fno-rtti ${CXXWARN} ${CTUNING} ${CMCU} ${RANDOM_DEFS}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu11 ${CWARN} ${CTUNING} ${CMCU} ${C_DEFS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++17 -Dregister=\"/**/\" -fno-exceptions -fno-rtti ${CXXWARN} ${CTUNING} ${CMCU} ${C_DEFS}")
 
 ReadVariables(${CMAKE_CURRENT_LIST_DIR}/Makefile "LDSCRIPT")      # get LD file
 set(PLATFORM_LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/${LDSCRIPT})
---
+++ targets/x86/unitTests/test_example.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(EXAMPLE, compare) {
+    EXPECT_EQ("string", "string");
+    EXPECT_EQ(1,1);
+}
